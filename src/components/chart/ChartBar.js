f525ef560b42e861cab0287972ce14255e42ac22
Mantas Santockis <mantas.san@gmail.com>
2023-07-24 09:42:55 +0300

    imptove code

--- src/components/chart/ChartBar.js
+++ src/components/chart/ChartBar.js
@@ -1,20 +1,23 @@
-import React from "react";
-import "./ChartBar.css";
+import React from 'react';
+
+import './ChartBar.css';
 
 const ChartBar = (props) => {
+  let barFillHeight = '0%';
 
-let barFillHeight = "0%"
-
-if(props.maxValue>0) {
-    barFillHeight=Math.round((props.value/props.MaxValue)*100)+ "%"
-}
+  if (props.maxValue > 0) {
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%';
+  }
 
   return (
-    <div className="chart-bar">
-      <div className="chat-bar__inner">
-        <div className="chat-bar__fill" style={{height: barFillHeight}}></div>
+    <div className='chart-bar'>
+      <div className='chart-bar__inner'>
+        <div
+          className='chart-bar__fill'
+          style={{ height: barFillHeight }}
+        ></div>
       </div>
-      <div className="chat-bar__label">{props.label}</div>
+      <div className='chart-bar__label'>{props.label}</div>
     </div>
   );
 };
